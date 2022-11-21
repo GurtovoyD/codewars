67bb2e85687537b701efac53a983868b627ed77c
GurtovoyD <d_gurtovoy@icloud.com>
2022-11-21 03:16:33 +0200

    20.11.2022 6kyu

---
+++ Character with longest consecutive repetition.py
@@ -0,0 +1,24 @@
+# For a given string s find the character c (or C) with longest consecutive repetition and return:
+#
+# (c, l)
+# where l (or L) is the length of the repetition. If there are two or more characters with the same
+# l return the first in order of appearance.
+#
+# For empty string return:
+#
+# ('', 0)
+# In JavaScript: If you use Array.sort in your solution, you might experience issues with the random tests as
+# Array.sort is not stable in the Node.js version used by CodeWars. This is not a kata issue.
+#
+# Happy coding! :)
+
+def longest_repetition(chars):
+    max_char, max_count = '', 0
+    char, count = '', 0
+    for c in chars:
+        if c != char:
+            count, char = 0, c
+        count += 1
+        if count > max_count:
+            max_char, max_count = char, count
+    return max_char, max_count
